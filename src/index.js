417277e5ae6f6c876a8e040a540bc0b1ed941770
angelvinyals <angel.vinyals@gmail.com>
2017-11-23 13:50:35 +0100

    provider update
    error. faltava acaba impot sentences

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import reducer from './reducers'
-import {provider} from 
+import {provider} from 'react-redux'
 
 const store = createStore(
   reducer,
