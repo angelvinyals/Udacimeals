a11eb68d01af890426885c29b21f30877464e7a5
angelvinyals <angel.vinyals@gmail.com>
2017-11-24 15:34:09 +0100

    combineReducers
    1I have created two reducer functions: food and calendar
    2 passed and object with both reducers into combinereducers
    3- updated mapStateProps function
    4-exported the return value of combineReducers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,22 +12,22 @@ class App extends Component {
     )
   }
 }
-function mapStateToProps (calendar) {
-  const diesDeLaSetmana = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-
-  return {
-    calendar: diesDeLaSetmana.map((day) => ({
-      day,
-      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-        meals[meal] = calendar[day][meal]
-          ? calendar[day][meal]
-          : null
-
-        return meals
-      }, {})
-    })),
+function mapStateToProps ({food,calendar}) {
+   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+ 
+   return {
+     calendar: dayOrder.map((day) => ({
+       day,
+       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+         meals[meal] = calendar[day][meal]
+           ? food[calendar[day][meal]]
+           : null
+ 
+         return meals
+       }, {})
+     })),
+    }
   }
-}
 
 function mapDispatchToProps(dispatch){
   return{
